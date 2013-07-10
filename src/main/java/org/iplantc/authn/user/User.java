3f47dab87e2260bf3907b839147644b16138ed36
Dennis Roberts <dennisroberts71@gmail.com>
2013-07-10 00:24:28 -0700

    CORE-4155: reformatted a class

--- src/main/java/org/iplantc/authn/user/User.java
+++ src/main/java/org/iplantc/authn/user/User.java
@@ -4,40 +4,40 @@ import net.sf.json.JSONObject;
 
 /**
  * Represents a Discovery Environment User.
+ *
  * @author Donald A. Barre
  */
 public class User {
 
-	private String username;
-	private String password;
+    private String username;
+    private String password;
     private String email;
     private String shortUsername;
     private String firstName;
     private String lastName;
 
-	public User() {
+    public User() {
+    }
 
-	}
+    public User(String username) {
+        this.username = username;
+    }
 
-	public User(String username) {
-		this.username = username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getEmail() {
         return email;
